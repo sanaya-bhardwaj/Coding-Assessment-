edf5085aa1fa9f8f5e2b26b1f99f3ba9485d94fe
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-06-05 19:51:55 +0530

    Update Training Ground V2.java

--- Training Ground V2.java
+++ Training Ground V2.java
@@ -1,83 +1,43 @@
-Hiko Seijuurou is looking for a suitable training ground for his students. He has a found a suitably perilous terrain of M×N
- cells. aij
- is the altitude of the cell on the i
--th row and the j
--th column.
-
-Seijuurou plans to drop students at any of the cells and ask them to visit at least K
- other cells. A student with capability c
- can only move to an adjacent cell (up/down/right/left) if the absolute difference in altitude between the two cells is at most c
-.
-
-For each cell, find the minimum capability a student needs to have to visit at least K
- other cells if they start at this cell.
-
-Input
-Three integers M,N,K
- on the first line.
-
-M
- lines follow with N
- integers on each line. The j
--th integer on the (i+1)
--th line is aij
-.Three integers M,N,K
- on the first line.
-
-Constraints
-
-1≤M,N≤500
-0≤K≤MN−1
-−109≤aij≤+109
-Output
-M
- lines with N
- integers on each line. The j
--th integer on the i
--th line should be the minimum capability a student needs to have to visit at least K
- other cells if they start at cell (i,j)
-.
-
-Examples
-input
-2 2 1
--3 -9
--8 2
-output
-5 6 
-5 10 
-input
-5 7 6
-7 6 -40 -11 -16 -28 -47
-37 -31 -19 -18 -13 -28 34
-14 -6 -46 0 -16 35 22
--34 -23 -42 -40 -27 11 -43
-9 5 -30 0 50 -32 0
-output
-30 30 21 11 11 12 19 
-23 12 11 11 11 12 38 
-20 19 13 16 11 38 38 
-19 19 13 13 11 38 43 
-28 28 13 30 50 43 43 
-Note
-In the first example, you need to reach only one other cell.
-
-From (1,1)
-, if you have 5
- capability, you can reach cell (2,1)
-.
-From (1,2)
-, if you have 6
- capability, you can reach cell (1,1)
-.
-From (2,1)
-, if you have 5
- capability, you can reach cell (1,1)
-.
-From (2,2)
-, if you have 10
- capability, you can reach cell (2,1)
-.
+//Hiko Seijuurou is looking for a suitable training ground for his students. He has a found a suitably perilous terrain of M×N cells. 
+ //aij is the altitude of the cell on the i-th row and the j-th column.
+//Seijuurou plans to drop students at any of the cells and ask them to visit at least K other cells. 
+//A student with capability c can only move to an adjacent cell (up/down/right/left) if the absolute difference in altitude between the two cells is at most c.
+//For each cell, find the minimum capability a student needs to have to visit at least K other cells if they start at this cell.
+//Input
+//Three integers M,N,K on the first line.
+//M lines follow with N integers on each line. The j-th integer on the (i+1)-th line is aij.Three integers M,N,K on the first line.
+//Constraints
+//1≤M,N≤500
+//0≤K≤MN−1−109≤aij≤+109
+//Output
+//M lines with N integers on each line. The j-th integer on the i-th line should be the minimum capability a student needs to have to visit at least K other cells if they start at cell (i,j).
+//Examples
+//input
+//2 2 1
+//-3 -9
+//-8 2
+//output
+//5 6 
+//5 10 
+//input
+//5 7 6
+//7 6 -40 -11 -16 -28 -47
+//37 -31 -19 -18 -13 -28 34
+//14 -6 -46 0 -16 35 22
+//-34 -23 -42 -40 -27 11 -43
+//9 5 -30 0 50 -32 0
+//output
+//30 30 21 11 11 12 19 
+//23 12 11 11 11 12 38 
+//20 19 13 16 11 38 38 
+//19 19 13 13 11 38 43 
+//28 28 13 30 50 43 43 
+//Note
+//In the first example, you need to reach only one other cell.
+//From (1,1), if you have 5 capability, you can reach cell (2,1).
+//From (1,2), if you have 6 capability, you can reach cell (1,1).
+//From (2,1), if you have 5 capability, you can reach cell (1,1).
+//From (2,2), if you have 10 capability, you can reach cell (2,1).
 
 import java.util.*;
 
